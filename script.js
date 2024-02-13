972c927775d55de6c3737651fc0f769ad8582026
manishsinghbisht10 <111435424+manishsinghbisht10@users.noreply.github.com>
2024-02-13 03:08:08 +0000

    adding listner on different condition

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 "use strict";
+const modal = document.querySelectorAll('.show-modal');
+const closeModel = document.querySelector('.close-modal');
 
-var modal = document.querySelectorAll(".show-modal");
 for (let i = 0; i < modal.length; i++) {
-  modal[i].addEventListener("click", function () {
-    document.querySelector(".hidden").style.display = "block";
+  modal[i].addEventListener('click', function () {
+    document.querySelector('.modal').classList.remove('hidden');
+    document.querySelector('.overlay').classList.remove('hidden');
   });
 }
+
+closeModel.addEventListener('click', function () {
+  document.querySelector('.modal').classList.add('hidden');
+  document.querySelector('.overlay').classList.add('hidden');
+});

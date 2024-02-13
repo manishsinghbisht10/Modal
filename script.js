ec866cf986e13943ae78e5cbb495255aa4dc9a92
manishsinghbisht10 <111435424+manishsinghbisht10@users.noreply.github.com>
2024-02-13 03:36:25 +0000

    event listner foe escape key

--- script.js
+++ script.js
@@ -18,3 +18,13 @@ document.querySelector('.overlay').addEventListener('click', function () {
     document.querySelector('.modal').classList.add('hidden');
     document.querySelector('.overlay').classList.add('hidden');
   });
+
+  document.addEventListener('keydown', function (event) {
+    if (
+      event.key === 'Escape' &&
+      !document.querySelector('.modal').classList.contains('hidden')
+    ) {
+      document.querySelector('.modal').classList.add('hidden');
+      document.querySelector('.overlay').classList.add('hidden');
+    }
+  });
